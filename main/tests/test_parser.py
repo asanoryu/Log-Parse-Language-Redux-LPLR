afad59ea3a3340050fb4734aa61ef0e40247f668
Vlado Shumanov <vl.shumanov@gmail.com>
2019-07-05 10:14:35 +0300

    test for parsing select_expression

--- main/tests/test_parser.py
+++ main/tests/test_parser.py
@@ -2,7 +2,7 @@
 import unittest
 from main.parser import lplr_parser
 from main.lexer import lexer
-from main.ast.lplr_ast import QueryStatement
+from main.ast.lplr_ast import QueryStatement, SelectExpr
 from main.lplr_tokens.lplr_token import Token
 
 
@@ -37,6 +37,17 @@ class TestParser(unittest.TestCase):
 
         self.assertIsNotNone(query.select_expression)
         self.assertEqual(query.select_expression.values, ["error"])
+
+    def test_parse_select_expression(self):
+        """Test parsing of a select expression."""
+        q = "error,warning"
+        p = lplr_parser.LPLRParser(input_=q, lex_generator=lexer.lex)
+        res = p.parse_get_stmt()
+
+        self.assertIsNotNone(res)
+        self.assertIsInstance(res, SelectExpr)
+
+        self.assertEqual(res.values, ["error", "warning"])
 
     def test_parse_get_stmt_multiple(self):
         """Parse a simple get statement."""
